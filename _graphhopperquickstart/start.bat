abbb6a404b51ade63622a3e0be091d3c46461b39
mgenz <73220980+mgenz@users.noreply.github.com>
2022-03-11 15:08:00 +0100

    Server.bat

---
+++ _graphhopperquickstart/start.bat
@@ -0,0 +1,5 @@
+cd U:\7_Semester\Navigation\graphhopperquickstart
+
+java -Ddw.graphhopper.datareader.file=brandenburg-latest.osm.pbf -jar graphhopper-web-5.0-SNAPSHOT.jar server config-example.yml
+
+PAUSE
