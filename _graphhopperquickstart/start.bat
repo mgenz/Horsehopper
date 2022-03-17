a96a138e98fbc3bca26cd054ca9f2ccda665076e
LFT-W47 <76479570+LFT-W47@users.noreply.github.com>
2022-03-17 16:47:13 +0100

    Delete _graphhopperquickstart directory

--- _graphhopperquickstart/start.bat
+++
@@ -1,5 +0,0 @@
-cd U:\7_Semester\Navigation\graphhopperquickstart
-
-java -Ddw.graphhopper.datareader.file=brandenburg-latest.osm.pbf -jar graphhopper-web-5.0-SNAPSHOT.jar server config-example.yml
-
-PAUSE
